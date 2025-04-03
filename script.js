19aa91b9652635151618023a6ae568fe6fd61232
Ranjeet <kiranjeetkr80@gmail.com>
2025-04-03 11:21:15 +0530

    final

--- script.js
+++ script.js
@@ -23,8 +23,7 @@ document.onkeydown = (e) => {
     if (
         e.key === "F12" ||
         (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "i") ||
-        (e.ctrlKey && e.key.toLowerCase() === "u") ||
-        (e.ctrlKey && e.key.toLowerCase() === "c")
+        (e.ctrlKey && e.key.toLowerCase() === "u")
     ) {
         alert("Action Blocked!");
         e.preventDefault();
